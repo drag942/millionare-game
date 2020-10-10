5a1dd297b0b143a8a2847c9da882c2a2964255c7
Dima <infermo94.09@gmail.com>
2020-10-10 21:26:37 +0300

    Updates

---
+++ precache-manifest.60d8ebf8eb376df3d93dd5060dd4f64f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ddd7a6e9a9b9a96c13e582df5bfaaa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e5c084c83fbeca17a66",
+    "url": "/static/css/main.a441ceb3.chunk.css"
+  },
+  {
+    "revision": "b634d63f533e227a4b60",
+    "url": "/static/js/2.02ce9225.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.02ce9225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e5c084c83fbeca17a66",
+    "url": "/static/js/main.c4aac1f0.chunk.js"
+  },
+  {
+    "revision": "254d524e2c3ae7e47892",
+    "url": "/static/js/runtime-main.108d45f4.js"
+  },
+  {
+    "revision": "36d0985d2e31182121a26e48dcb86573",
+    "url": "/static/media/hand.36d0985d.svg"
+  }
+]);
