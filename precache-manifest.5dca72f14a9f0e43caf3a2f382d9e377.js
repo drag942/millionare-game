c4d8865e17c9f310b648ea2672d1bbf2cf6ec550
Dima <infermo94.09@gmail.com>
2020-10-10 21:34:26 +0300

    Updates

---
+++ precache-manifest.5dca72f14a9f0e43caf3a2f382d9e377.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4e1f870d03f37f5126f00c44e84f59",
+    "url": "/millionare-game/index.html"
+  },
+  {
+    "revision": "3e5c084c83fbeca17a66",
+    "url": "/millionare-game/static/css/main.a441ceb3.chunk.css"
+  },
+  {
+    "revision": "b634d63f533e227a4b60",
+    "url": "/millionare-game/static/js/2.02ce9225.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/millionare-game/static/js/2.02ce9225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e5c084c83fbeca17a66",
+    "url": "/millionare-game/static/js/main.c4aac1f0.chunk.js"
+  },
+  {
+    "revision": "0f02d8dbcf1a71dea70f",
+    "url": "/millionare-game/static/js/runtime-main.95bef59d.js"
+  },
+  {
+    "revision": "36d0985d2e31182121a26e48dcb86573",
+    "url": "/millionare-game/static/media/hand.36d0985d.svg"
+  }
+]);
